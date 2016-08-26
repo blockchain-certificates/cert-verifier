4df49b27587617d1b496295f5340666bd95fe5b5
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-08-25 17:46:24 -0700

    update verification with new schema

--- verify.py
+++ verify.py
@@ -9,7 +9,7 @@ from bitcoin.signmessage import BitcoinMessage, VerifyMessage
 from cert_schema.schema_tools import schema_validator
 from merkleproof import utils
 from merkleproof.MerkleTree import sha256
-
+from pyld import jsonld
 
 unhexlify = binascii.unhexlify
 hexlify = binascii.hexlify
@@ -42,20 +42,23 @@ def lookup_transaction(transaction_id):
 
 
 
-def verify_v2(signed_local_json, signed_local_file):
+def verify_v2(signed_local_json):
     verify_response = []
     verified = False
 
 
-    valid_proof = utils.validate_proof(signed_local_json['blockchainreceipt']['proof'],
-                         signed_local_json['blockchainreceipt']['targetHash'],
-                         signed_local_json['blockchainreceipt']['merkleRoot'], sha256)
+    valid_proof = utils.validate_proof(signed_local_json['receipt']['proof'],
+                        signed_local_json['receipt']['targetHash'],
+                        signed_local_json['receipt']['merkleRoot'], sha256)
     print(valid_proof)
 
     # TODO: validation should now happen through normalized form
 
-    transaction_info = None
-    if not transaction_info:
+
+
+    transaction_id = signed_local_json['receipt']['anchors'][0]['sourceId']
+    transaction_data = lookup_transaction(transaction_id)
+    if not transaction_data:
         verify_response.append(('Looking up by transaction_id', False))
         verify_response.append(("Verified", False))
         return verify_response
@@ -65,14 +68,20 @@ def verify_v2(signed_local_json, signed_local_file):
         ("Computing SHA256 digest of local certificate", "DONE"))
     verify_response.append(("Fetching hash in OP_RETURN field", "DONE"))
 
-    signer_url = signed_local_json['certificate']['issuer']['id']
-    uid = signed_local_json['assertion']['uid']
-    signature = signed_local_json['signature']
+    signer_url = signed_local_json['document']['certificate']['issuer']['id']
+    uid = signed_local_json['document']['assertion']['uid']
+    signature = signed_local_json['document']['signature']
 
     # compare hashes
-    compare_hash_result = compare_hashes(signed_local_file, transaction_info)
+    target_hash = signed_local_json['receipt']['targetHash']
+    compare_hash_result = compare_hashes_v2(signed_local_json, target_hash)
     verify_response.append(
         ("Comparing local and blockchain hashes", compare_hash_result))
+
+    # check merkle root
+    merkle_root = signed_local_json['receipt']['merkleRoot']
+    remote_hash = get_hash_from_bc_op(transaction_data)
+    compare_hash_result = _compare_hashes(merkle_root, remote_hash)
 
     keys = get_issuer_keys(signer_url)
     signing_key = keys['issuer_key'][0]['key']
@@ -83,7 +92,7 @@ def verify_v2(signed_local_json, signed_local_file):
     verify_response.append(("Checking signature", author_verified))
 
     # check if it's been revoked by the issuer
-    not_revoked = check_revocation(transaction_info, revocation_address)
+    not_revoked = check_revocation(transaction_data, revocation_address)
     verify_response.append(("Checking not revoked by issuer", not_revoked))
 
     if compare_hash_result and author_verified and not_revoked:
@@ -110,9 +119,9 @@ def verify_v1(transaction_id, signed_local_json, signed_local_file):
         ("Computing SHA256 digest of local certificate", "DONE"))
     verify_response.append(("Fetching hash in OP_RETURN field", "DONE"))
 
-    signer_url = signed_local_json['certificate']['issuer']['id']
-    uid = signed_local_json['assertion']['uid']
-    signature = signed_local_json['signature']
+    signer_url = signed_local_json['document']['certificate']['issuer']['id']
+    uid = signed_local_json['document']['assertion']['uid']
+    signature = signed_local_json['document']['signature']
 
     # compare hashes
     compare_hash_result = compare_hashes(signed_local_file, transaction_info)
@@ -145,9 +154,14 @@ def check_issuer_signature(signing_key, uid, signature):
     return VerifyMessage(signing_key, message, signature)
 
 
+def compare_hashes_v2(signed_local_json, target_hash):
+    local_hash = compute_v2_hash(signed_local_json)
+    compare_hash_result = _compare_hashes(local_hash, target_hash)
+    return compare_hash_result
+
 
 def compare_hashes(signed_local_file, transaction_info):
-    local_hash = compute_hash(signed_local_file)
+    local_hash = compute_v1_hash(signed_local_file)
     remote_hash = get_hash_from_bc_op(transaction_info)
     compare_hash_result = _compare_hashes(local_hash, remote_hash)
     return compare_hash_result
@@ -186,12 +200,21 @@ def check_revocation(tx_json, revoke_address):
     return False
 
 
-def compute_hash(doc):
+def compute_v1_hash(doc):
     doc_bytes = doc
     if not isinstance(doc, (bytes, bytearray)):
         doc_bytes = doc.encode('utf-8')
     return hashlib.sha256(doc_bytes).hexdigest()
 
+
+def compute_v2_hash(signed_local_json):
+    normalized = jsonld.normalize(cert_json, {'algorithm': 'URDNA2015', 'format': 'application/nquads'})
+    hashed = sha256(normalized)
+    return hashed
+
+def do_hash_certificate(self, certificate):
+
+    return hashed
 
 def _compare_hashes(hash1, hash2):
     if hash1 in hash2 or hash1 == hash2:

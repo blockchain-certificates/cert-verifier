51aa03010c9c3507e54d029da8ab6525b1db99c0
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-08-29 11:48:26 -0700

    update samples and switch to package

--- cert_verifier/verify.py
+++ cert_verifier/verify.py
@@ -1,16 +1,18 @@
 import binascii
+import hashlib
 import json
 import logging
 import sys
 
-import hashlib
+import bitcoin
 import requests
 from bitcoin.signmessage import BitcoinMessage, VerifyMessage
 from cert_schema.schema_tools import schema_validator
 from merkleproof import utils
 from merkleproof.MerkleTree import sha256
 from pyld import jsonld
-from connectors import BlockcypherConnector
+
+from cert_verifier.connectors import BlockcypherConnector
 
 unhexlify = binascii.unhexlify
 hexlify = binascii.hexlify
@@ -36,10 +38,7 @@ def check_issuer_signature(signing_key, uid, signature):
     return VerifyMessage(signing_key, message, signature)
 
 
-def compute_v1_hash(doc):
-    doc_bytes = doc
-    if not isinstance(doc, (bytes, bytearray)):
-        doc_bytes = doc.encode('utf-8')
+def compute_v1_hash(doc_bytes):
     return hashlib.sha256(doc_bytes).hexdigest()
 
 
@@ -66,8 +65,14 @@ def get_issuer_keys(signer_url):
     return remote_json
 
 
-def verify_v2(signed_local_json):
-    connector = BlockcypherConnector('testnet')
+def verify_v1_2(signed_local_json, chain=None):
+    if chain:
+        connector = BlockcypherConnector(chain)
+        bitcoin.SelectParams(chain)
+
+    # first ensure this is a valid v1.2 cert
+    schema_validator.validate_v1_2_0(cert_json)
+
     verify_response = []
 
     # check the proof before doing anything else
@@ -112,6 +117,7 @@ def verify_v2(signed_local_json):
     keys = get_issuer_keys(signer_url)
     signing_key = keys['issuer_key'][0]['key']
     revocation_address = keys['revocation_key'][0]['key']
+
     author_verified = check_issuer_signature(signing_key, uid, signature)
     verify_response.append(("Checking signature", author_verified))
 
@@ -124,8 +130,17 @@ def verify_v2(signed_local_json):
     return verify_response
 
 
-def verify_v1(transaction_id, signed_local_json, signed_local_file):
-    connector = BlockcypherConnector('testnet')
+def verify_v1_1(transaction_id, cert_file_bytes, chain=None):
+    if chain:
+        connector = BlockcypherConnector(chain)
+        bitcoin.SelectParams(chain)
+
+    cert_utf8 = cert_file_bytes.decode('utf-8')
+    cert_json = json.loads(cert_utf8)
+
+    # first ensure this is a valid v1.1 cert
+    schema_validator.validate_v1_1_0(cert_json)
+
     verify_response = []
 
     transaction_info = connector.lookup_tx(transaction_id)
@@ -138,7 +153,7 @@ def verify_v1(transaction_id, signed_local_json, signed_local_file):
     verify_response.append(("Fetching hash in OP_RETURN field", "DONE"))
 
     # compute local hash
-    local_hash = compute_v1_hash(signed_local_file)
+    local_hash = compute_v1_hash(cert_file_bytes)
     verify_response.append(("Computing SHA256 digest of local certificate", "DONE"))
 
     # compare hashes
@@ -146,9 +161,9 @@ def verify_v1(transaction_id, signed_local_json, signed_local_file):
     verify_response.append(("Comparing local and blockchain hashes", compare_hash_result))
 
     # check author
-    signer_url = signed_local_json['document']['certificate']['issuer']['id']
-    uid = signed_local_json['document']['assertion']['uid']
-    signature = signed_local_json['document']['signature']
+    signer_url = cert_json['certificate']['issuer']['id']
+    uid = cert_json['assertion']['uid']
+    signature = cert_json['signature']
 
     keys = get_issuer_keys(signer_url)
     signing_key = keys['issuer_key'][0]['key']
@@ -167,18 +182,12 @@ def verify_v1(transaction_id, signed_local_json, signed_local_file):
 
 if __name__ == "__main__":
 
-    # TODO: validate before running
-
-    #schema_updater.update('sample_data/1.1.0/sample_signed_cert-1.1.0.json',
-    #                      'sample_data/1.2.0/sample_signed_cert-1.2.0.json-new')
-
     with open('sample_data/1.2.0/sample_signed_cert-1.2.0.json') as cert_file:
         cert_json = json.load(cert_file)
-        schema_validator.validate_v1_2_0(cert_json)
-        result = verify_v2(cert_json)
+        result = verify_v1_2(cert_json, 'testnet')
         print(result)
 
-
     with open('sample_data/1.1.0/sample_signed_cert-1.1.0.json', 'rb') as cert_file:
-        result = verify_v1('c5e240974c3ab4dc0d85bb28cb8a7c87a1a250532b8f985e3960ef4a23244a6a', cert_json, cert_file.read())
+        result = verify_v1_1('1703d2f5d706d495c1c65b40a086991ab755cc0a02bef51cd4aff9ed7a8586aa',
+                             cert_file.read(), 'testnet')
         print(result)
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import uuid
+
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+install_reqs = parse_requirements(os.path.join(here, 'requirements.txt'), session=uuid.uuid1())
+reqs = [str(ir.req) for ir in install_reqs]
+
+with open(os.path.join(here, 'README.md')) as fp:
+    long_description = fp.read()
+
+setup(
+    name='cert-verifier',
+    version='1.2.0',
+    description='verifies blockchain certificates',
+    author='MIT Media Lab Digital Certificates',
+    tests_require=['tox'],
+    url='https://github.com/digital-certificates/cert-verifier',
+    license='MIT',
+    author_email='certs@media.mit.edu',
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=reqs
+)

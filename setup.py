833596bf1e45e608e9c13cf65e6978eb92ef68fc
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-09-10 10:25:41 -0700

    increase version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-verifier',
-    version='1.2.0',
+    version='1.2.1',
     description='verifies blockchain certificates',
     author='MIT Media Lab Blockchain Certificates',
     tests_require=['tox'],

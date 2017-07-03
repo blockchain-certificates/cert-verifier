ae24ac14a87db885e98ff33f5d39fd7c17fd50d5
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-07-03 14:26:57 -0700

    consume latest cert-schema with enum34 fix and fix file name in test

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-verifier',
-    version='2.0',
+    version='2.0.1',
     description='Verifies blockchain certificates',
     author='Blockcerts',
     tests_require=['tox'],

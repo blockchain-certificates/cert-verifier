46da4394c2b57e2f2e128fc154c25efe3647ac9c
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-10-23 10:25:04 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-verifier',
-    version='2.0.9',
+    version='2.0.10',
     description='Verifies blockchain certificates',
     author='Blockcerts',
     tests_require=['tox'],
